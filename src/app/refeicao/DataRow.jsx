c088b9c8fb32ad11b42c5046f6a90be2c3daa522
Ingridviieria <rm95396@fiap.com.br>
2023-11-08 17:38:05 -0300

    estilizado

--- src/app/refeicao/DataRow.jsx
+++ src/app/refeicao/DataRow.jsx
@@ -1,15 +1,27 @@
+import './styles.css';
+
 export default function DataRow({ refeicao }) {
     console.log('ID da Refeicao:', refeicao.id);
 
     return (
-        <div id="data-row" className="group flex items-center justify-between p-2 my-2 rounded cursor-pointer bg-gray-200"> {/* Adicionando fundo cinza claro */}
-            <div className="flex gap-4 items-center h-96 w-full overflow-y-auto">
-                <div className="bg-white rounded p-4 flex flex-col flex-1"> {/* Adicionando fundo branco e usando flex-1 para preencher o espaço */}
-                    <h1 className="text-2xl underline">Nome da Refeição</h1>
-                    <span className="text-2xl text-black">{refeicao.nome}</span>
-                    <h1 className="text-2xl underline">Nome da Refeição</h1>
-                    <span className="text-2xl text-black">{refeicao.nomeRefeicao}</span>
-                </div>
+        <div id="data-row">
+            <div className="group flex items-center justify-between p-2 my-2 rounded cursor-pointer bg-gray-200">
+                    <div className="flex flex-row">
+                        <h1 className="mr-2 large-text">Nome: </h1>
+                        <span className="large-text">{refeicao.nome}</span>
+                    </div>
+
+                    <div className="flex flex-row">
+                        <h1 className="mr-2 large-text">Nome da Refeição: </h1>
+                        <span className="large-text">{refeicao.nomeRefeicao}</span>
+                    </div>
+
+                    {refeicao.motivo && (
+                        <div className="flex flex-row">
+                            <h1 className="mr-2 large-text">Motivo: </h1>
+                            <span className="large-text">{refeicao.motivo.descricaoMotivo}</span>
+                        </div>
+                    )}
             </div>
         </div>
     );
